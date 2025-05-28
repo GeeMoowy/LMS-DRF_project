364ef9eaec847031bdfb2b80791664550e595e4b
geemoowy <kovylek.ul@gmail.com>
2025-05-28 22:45:32 +0300

    add CourseViewSet and serializer

---
+++ lms/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+
+from lms.models import Course
+
+
+class CourseSerializer(ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
